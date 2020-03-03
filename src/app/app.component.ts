341fba92c56c9495ce37bea343332a367b67c529
Dang Huu Canh <huucanh2092@gmail.com>
2020-03-03 22:43:17 +0700

    Add bootstrap with kendo-ui example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SchedulerEvent } from '@progress/kendo-angular-scheduler';
+import { sampleData, displayDate } from './events-utc';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bootstrap-example';
+  public selectedDate: Date = displayDate;
+  public events: SchedulerEvent[] = sampleData;
 }
